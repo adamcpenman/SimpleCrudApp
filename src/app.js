ffc83d6dedaf3099e2173fa4fbe41de909c1f368
Adam <50333588+adamcpenman@users.noreply.github.com>
2019-10-01 12:02:47 -0500

    edit button
    have a working edit button

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import UserTable from "./tables/UserTable";
 import AddUserForm from "./forms/AddUserForm";
+import EditUserForm from "./forms/EditUserForm";
 
 const App = () => {
   const usersData = [
@@ -16,17 +17,51 @@ const App = () => {
     setUsers([...users, user]);
   };
 
+  const deleteUser = id => {
+    setUsers(users.filter(user => user.id !== id));
+  };
+
+  const [editing, setEditing] = useState(false);
+  const initialFormState = { id: null, name: "", username: "" };
+  const [currentUser, setCurrentUser] = useState(initialFormState);
+
+  const editRow = user => {
+    setEditing(true);
+    console.log(user);
+    setCurrentUser({ id: user.id, name: user.name, username: user.username });
+  };
+
+  const updateUser = (id, updatedUser) => {
+    setEditing(false);
+
+    setUsers(users.map(user => (user.id === id ? updatedUser : user)));
+  };
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Add user</h2>
-          <AddUserForm addUser={addUser} />
+          {editing ? (
+            <div>
+              <h2>Edit User</h2>
+              <EditUserForm
+                editing={editing}
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updateUser={updateUser}
+              />
+            </div>
+          ) : (
+            <div>
+              <h2>Add user</h2>
+              <AddUserForm addUser={addUser} />
+            </div>
+          )}
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable users={users} />
+          <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
         </div>
       </div>
     </div>
